38db40dd4095400914b4da579797811ddcdffb99
ShunjiroOsada <shunjiroosada@gmail.com>
2020-04-27 22:05:22 +0900

    add some files for init-workspace setup

---
+++ scripts/init_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt-get install git
+sudo apt-get install emacs
+sudo apt-get install terminator
+sudo apt-get install gnome-system-monitor
+sudo apt-get install clisp
+cd $HOME
+echo "emacs setting file install"
+git clone https://github.com/ShunjiroOsada/.emacs.d.git
+echo "SSH-key generate"
+mkdir .ssh
+ssh-keygen -t rsa
+echo "Please use this ssh-key for GitHub"
