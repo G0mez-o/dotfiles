76afc5bceb7038bfc12e52129b479226e3a58df2
ShunjiroOsada <39079482+ShunjiroOsada@users.noreply.github.com>
2020-05-19 10:44:21 +0900

    Update init_setup.sh

--- scripts/init_setup.sh
+++ scripts/init_setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 sudo apt-get install cmake
-sudo apt-get install emacs
+sudo add-apt-repository ppa:kelleyk/emacs
+sudo apt-get update
+sudo apt-get install emacs26
 sudo apt-get install terminator
 sudo apt-get install gnome-system-monitor
 sudo apt-get install clisp
@@ -12,3 +14,4 @@ echo "SSH-key generate"
 mkdir .ssh
 ssh-keygen -t rsa
 echo "Please use this ssh-key for GitHub"
+emacs26
